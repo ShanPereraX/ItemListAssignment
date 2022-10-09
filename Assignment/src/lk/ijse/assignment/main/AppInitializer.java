a08fea64983782d128962ca70bacd3e7c66fb66c
Lakshan Chamoditha Perera <103528441+ShanPereraX@users.noreply.github.com>
2022-10-09 18:51:37 +0530

    Add files via upload

---
+++ Assignment/src/lk/ijse/assignment/main/AppInitializer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.ijse.assignment.main;
+
+import lk.ijse.assignment.view.MainForm;
+/**
+ *
+ * @author shan
+ */
+public class AppInitializer {
+    public static void main(String[] args) {
+        new MainForm().setVisible(true);
+    }
+}
